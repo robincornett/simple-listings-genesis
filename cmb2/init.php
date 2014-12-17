3a5426e299fba9d01325649acef4314b4638117c
Robin Cornett <hello@robincornett.com>
2014-12-17 17:55:25 -0500

    remove CMB2/WP header
    Since it's inside SImple Listings, the WP header is causing an activation error.

--- cmb2/init.php
+++ cmb2/init.php
@@ -1,23 +1,5 @@
 <?php
 /**
- * Plugin Name:  CMB2 (beta)
- * Plugin URI:   https://github.com/WebDevStudios/CMB2
- * Description:  CMB2 will create metaboxes and forms with custom fields that will blow your mind.
- * Author:       WebDevStudios
- * Author URI:   http://webdevstudios.com
- * Contributors: WebDevStudios (@webdevstudios / webdevstudios.com)
- *               Justin Sternberg (@jtsternberg / dsgnwrks.pro)
- *               Jared Atchison (@jaredatch / jaredatchison.com)
- *               Bill Erickson (@billerickson / billerickson.net)
- *               Andrew Norcross (@norcross / andrewnorcross.com)
- *
- * Version:      2.0.0.3
- *
- * Text Domain:  cmb2
- * Domain Path:  languages
- */
-
-/**
  * Released under the GPL license
  * http://www.opensource.org/licenses/gpl-license.php
  *

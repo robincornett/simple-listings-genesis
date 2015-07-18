6044a67b2062394644157c1cc9f50abd6c741c3b
Robin Cornett <hello@robincornett.com>
2015-07-18 15:50:08 -0400

    Refactor archive page
    Totally simplified, still works for XHTML

--- views/archive-listing.php
+++ views/archive-listing.php
@@ -16,65 +16,22 @@ function simplelisting_body_class( $classes ) {
 }
 
 remove_action( 'genesis_loop', 'genesis_do_loop' );
-add_action( 'genesis_loop', 'simplelisting_archive_loops' );
+add_action( 'genesis_loop', 'simplelistinggenesis_archive_loop' );
 
 /**
- * Chooses which archive loop to run, HTML5 or XHTML.
- *
- * @since  1.3.0
- *
- * @return genesis archive loop
+ * Custom archive loop
+ * @since 1.5.0
  */
-function simplelisting_archive_loops() {
-	if ( genesis_html5() ) {
-		simplelisting_loop_html5();
-	}
-	else {
-		simplelisting_loop_xhtml();
-	}
-}
-
-/**
- * Simple Listings HTML5 loop
- *
- * @since  1.3.0
- *
- * @return HTML5 loop
- */
-function simplelisting_loop_html5() {
+function simplelistinggenesis_archive_loop() {
 
 	echo '<div class="listings">';
 	if ( have_posts() ) :
 
 		while ( have_posts() ) : the_post();
-
-		$status = get_the_term_list( get_the_ID(), 'status', '', ', ', '' ); ?>
-
-			<article <?php post_class(); ?>><div class="listing-wrap"> <?php
-				$image    = get_the_post_thumbnail( get_the_ID(), 'listing-photo', array( 'class' => 'aligncenter', 'alt' => get_the_title(), 'title' => get_the_title() ) );
-				$fallback = plugins_url( 'includes/sample-images/simple-listings.png' , dirname( __FILE__ ) );
-
-				if ( $image ) {
-					echo '<a href="' . get_permalink() . '">' . $image;
-				}
-				else {
-					printf(
-						'<a href="%s"><img src="%s" class="aligncenter" alt="%s" />',
-						get_permalink(),
-						$fallback,
-						the_title_attribute( 'echo=0' )
-					);
-				}
-
-				echo genesis_html5() ? '<header class="entry-header">' : '';
-				printf( '<h2 class="entry-title">%s</h2>', the_title_attribute( 'echo=0' ) );
-
-				if ( $status ) {
-					echo '<span class="listing-status">' . strip_tags( $status ) . '</span>';
-				}
-				echo genesis_html5() ? '</header>' : '';
-
-			echo '</a></div></article>';
+			$article = genesis_html5() ? 'article' : 'div';
+			printf( '<%s class="%s">', esc_attr( $article ), esc_attr( join( ' ', get_post_class() ) ) );
+				simplelistingsgenesis_archive_photo();
+			printf( '</%s>', esc_attr( $article ) );
 
 		endwhile;
 		do_action( 'genesis_after_endwhile' );
@@ -83,50 +40,37 @@ function simplelisting_loop_html5() {
 }
 
 /**
- * Simple Listings XHTML loop
+ * Photo for archive
+ * @return image with link, title, and status
  *
- * @since  1.3.0
- *
- * @return XHTML loop
+ * @since 1.5.0
  */
-function simplelisting_loop_xhtml() {
+function simplelistingsgenesis_archive_photo() {
+	echo '<div class="listing-wrap">';
+	$status   = get_the_term_list( get_the_ID(), 'status', '', ', ', '' );
+	$image    = get_the_post_thumbnail( get_the_ID(), 'listing-photo', array( 'class' => 'aligncenter', 'alt' => get_the_title(), 'title' => get_the_title() ) );
+	$fallback = plugins_url( 'includes/sample-images/simple-listings.png' , dirname( __FILE__ ) );
 
-	echo '<div class="listings">';
-	if ( have_posts() ) :
+	if ( $image ) {
+		echo '<a href="' . esc_url( get_permalink() ) . '">' . wp_kses_post( $image );
+	} else {
+		printf(
+			'<a href="%s"><img src="%s" class="aligncenter" alt="%s" />',
+			esc_url( get_permalink() ),
+			esc_url( $fallback ),
+			the_title_attribute( 'echo=0' )
+		);
+	}
 
-		while ( have_posts() ) : the_post();
+	echo genesis_html5() ? '<header class="entry-header">' : '';
+	printf( '<h2 class="entry-title">%s</h2>', the_title_attribute( 'echo=0' ) );
 
-		$status = get_the_term_list( get_the_ID(), 'status', '', ', ', '' );
+	if ( $status ) {
+		echo '<span class="listing-status">' . esc_attr( strip_tags( $status ) ) . '</span>';
+	}
+	echo genesis_html5() ? '</header>' : '';
 
-			printf( '<div class="%s">', join( ' ', get_post_class() ) );
-			echo '<div class="listing-wrap">';
-				$image    = get_the_post_thumbnail( get_the_ID(), 'listing-photo', array( 'class' => 'aligncenter', 'alt' => get_the_title() ) );
-				$fallback = plugins_url( 'includes/sample-images/simple-listings.png' , dirname( __FILE__ ) );
-
-				if ( $image ) {
-					echo '<a href="' . get_permalink() . '">' . $image;
-				}
-				else {
-					printf(
-						'<a href="%s"><img src="%s" class="aligncenter" alt="%s" />',
-						get_permalink(),
-						$fallback,
-						the_title_attribute( 'echo=0' )
-					);
-				}
-
-				printf( '<h2 class="entry-title">%s</h2>', the_title_attribute( 'echo=0' ) );
-
-				if ( $status ) {
-					echo '<span class="listing-status">' . strip_tags( $status ) . '</span>';
-				}
-
-			echo '</a></div></div>';
-
-		endwhile;
-		do_action( 'genesis_after_endwhile' );
-	endif;
-	echo '</div>';
+	echo '</a></div>';
 }
 
 genesis();
